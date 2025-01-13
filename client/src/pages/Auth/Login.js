3dbe1a0da5e568edf07544d69969a09c861a1dda
Pratham-011 <pratham.ds02@gmail.com>
2025-01-13 22:01:10 +0530

    made some changes

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Layout from '../../components/Layout/Layout'
 import toast from 'react-hot-toast';
-
+// import './Login.css'
 import axios from 'axios'
 import { useNavigate,useLocation } from 'react-router-dom'
 import "../../styles/AuthStyles.css"
